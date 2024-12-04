e1257ec9561f77f5a8d47bf08ab8addde1800d4b
dakhli <ahemddakhli99@gmail.com>
2024-12-04 16:38:09 +0200

    edit provider

--- src/Providers/BlogsServiceProvider.php
+++ src/Providers/BlogsServiceProvider.php
@@ -35,11 +35,11 @@ class BlogsServiceProvider extends ServiceProvider
 
         // Load translations
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'blog');
-        $this->registerCommands();
-        $this->registerCommandSchedules();
-        $this->registerTranslations();
-        $this->registerConfig();
-        $this->registerViews();
+        // $this->registerCommands();
+        // $this->registerCommandSchedules();
+        // $this->registerTranslations();
+        // $this->registerConfig();
+        // $this->registerViews();
         // $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
     }
 
@@ -48,8 +48,8 @@ class BlogsServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->register(EventServiceProvider::class);
-        $this->app->register(RouteServiceProvider::class);
+        // $this->app->register(EventServiceProvider::class);
+        // $this->app->register(RouteServiceProvider::class);
     }
 
     /**
@@ -76,15 +76,15 @@ class BlogsServiceProvider extends ServiceProvider
      */
     public function registerTranslations(): void
     {
-        $langPath = resource_path('lang/modules/' . $this->nameLower);
+        // $langPath = resource_path('lang/modules/' . $this->nameLower);
 
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->nameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->name, 'lang'), $this->nameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->name, 'lang'));
-        }
+        // if (is_dir($langPath)) {
+        //     $this->loadTranslationsFrom($langPath, $this->nameLower);
+        //     $this->loadJsonTranslationsFrom($langPath);
+        // } else {
+        //     $this->loadTranslationsFrom(module_path($this->name, 'lang'), $this->nameLower);
+        //     $this->loadJsonTranslationsFrom(module_path($this->name, 'lang'));
+        // }
     }
 
     /**
@@ -92,8 +92,8 @@ class BlogsServiceProvider extends ServiceProvider
      */
     protected function registerConfig(): void
     {
-        $this->publishes([module_path($this->name, 'config/config.php') => config_path($this->nameLower . '.php')], 'config');
-        $this->mergeConfigFrom(module_path($this->name, 'config/config.php'), $this->nameLower);
+        // $this->publishes([module_path($this->name, 'config/config.php') => config_path($this->nameLower . '.php')], 'config');
+        // $this->mergeConfigFrom(module_path($this->name, 'config/config.php'), $this->nameLower);
     }
 
     /**
@@ -101,15 +101,15 @@ class BlogsServiceProvider extends ServiceProvider
      */
     public function registerViews(): void
     {
-        $viewPath = resource_path('views/modules/' . $this->nameLower);
-        $sourcePath = module_path($this->name, 'resources/views');
+        // $viewPath = resource_path('views/modules/' . $this->nameLower);
+        // $sourcePath = module_path($this->name, 'resources/views');
 
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->nameLower . '-module-views']);
+        // $this->publishes([$sourcePath => $viewPath], ['views', $this->nameLower . '-module-views']);
 
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->nameLower);
+        // $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->nameLower);
 
-        $componentNamespace = $this->module_namespace($this->name, $this->app_path(config('modules.paths.generator.component-class.path')));
-        Blade::componentNamespace($componentNamespace, $this->nameLower);
+        // $componentNamespace = $this->module_namespace($this->name, $this->app_path(config('modules.paths.generator.component-class.path')));
+        // Blade::componentNamespace($componentNamespace, $this->nameLower);
     }
 
     /**
@@ -123,11 +123,11 @@ class BlogsServiceProvider extends ServiceProvider
     private function getPublishableViewPaths(): array
     {
         $paths = [];
-        foreach (config('view.paths') as $path) {
-            if (is_dir($path . '/modules/' . $this->nameLower)) {
-                $paths[] = $path . '/modules/' . $this->nameLower;
-            }
-        }
+        // foreach (config('view.paths') as $path) {
+        //     if (is_dir($path . '/modules/' . $this->nameLower)) {
+        //         $paths[] = $path . '/modules/' . $this->nameLower;
+        //     }
+        // }
 
         return $paths;
     }
